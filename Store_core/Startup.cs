28bcd5020f5f6a8eb800588578e8f0e892f9042a
Mirisan <ktotovkaske@gmail.com>
2022-01-14 23:07:26 +0300

    Project markup

--- Store_core/Startup.cs
+++ Store_core/Startup.cs
@@ -16,6 +16,8 @@ namespace Store_core
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +28,13 @@ namespace Store_core
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
+            app.UseRouting(); // используем систему маршрутизации
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
